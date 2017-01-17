63903a1e6a73730e3bb4cc8466c3b4310b43dedb
munichrocker <b.witzenberger@googlemail.com>
2017-01-17 18:34:21 +0100

    Initializing Script, loading data, specifying Column-Names and Types

---
+++ platzfinder.R
@@ -0,0 +1,7 @@
+library(dplyr)
+library(ggplot2)
+
+colN <- c("Bibliothek", "Belegt", "Leer", "Datum")
+d <- read.csv2("data.csv", stringsAsFactors = TRUE, col.names = colN, colClasses = c("factor", "numeric", "numeric", "POSIXct"), header = FALSE, encoding = "UTF-8", na.strings = "Geschlossen")
+
+
