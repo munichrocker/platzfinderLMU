b8386dee06fb3c51bc27c72a384b38e2ac7fd10c
munichrocker <b.witzenberger@googlemail.com>
2017-01-17 19:10:02 +0100

    removed test data and created new variables: weekday and hour

--- platzfinder.R
+++ platzfinder.R
@@ -1,7 +1,11 @@
 library(dplyr)
 library(ggplot2)
+library(lubridate)
 
 colN <- c("Bibliothek", "Belegt", "Leer", "Datum")
 d <- read.csv2("data.csv", stringsAsFactors = TRUE, col.names = colN, colClasses = c("factor", "numeric", "numeric", "POSIXct"), header = FALSE, encoding = "UTF-8", na.strings = "Geschlossen")
 
-
+#Remove Test-Data and create new columns
+d %>% 
+  filter(Datum > "2016-12-17 23:45:29") %>% 
+  mutate(weekday = weekdays.POSIXt(Datum), hour = hour(Datum)) -> w
